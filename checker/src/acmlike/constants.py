1465a6e61bef9a988c84493cb1a1a2df1cdec35d
Arthur Pilone <arthurpilone@ime.usp.br>
2026-08-01 10:35:21 -0300

    Add verb env font to acm template fonts
    Signed-off-by: Arthur Pilone <arthurpilone@ime.usp.br>

--- checker/src/acmlike/constants.py
+++ checker/src/acmlike/constants.py
@@ -3,12 +3,12 @@ Houses the definition of constants specific to the ACMlike template checker.
 """
 
 #MAIN_FONT = "LinLibertineT"
-PAGE_HEADER_FONT = ("LinBiolinumT", "LinBiolinumO", "CMSS8", "LMSans8-Regular")  #Including 'wrong' fonts
+PAGE_HEADER_FONT = ("LinBiolinumT", "LinBiolinumO", "Inconsolatazi4-Regular", "CMSS8", "LMSans8-Regular")  #Including 'wrong' fonts;
 ABSTRACT_FONT = "LinLibertineT"
 AUTHOR_BLOCK_FONT = ("LinLibertineT", "LinLibertineO" "CMR12", "LMRoman12-Regular")
 SECTION_TITLE_FONT = ("LinLibertineTB","LinLibertineOB","CMBX10", "LMRomanDemi10-Regular") #Including 'wrong' fonts
 #TABLE_CAPTION_FONT = "LinLibertineTB"
-CORRECT_TEMPLATE_FONT = ("LinBiolinumT", "LinBiolinumO", "LinLibertineT", "LinLibertineO", "LinLibertineTB", "LinLibertineOB", "LinLibertineTI", "LinLibertineOI", "LibertineMathMI")
+CORRECT_TEMPLATE_FONT = ("LinBiolinumT", "LinBiolinumO", "LinLibertineT", "LinLibertineO", "LinLibertineTB", "LinLibertineOB", "LinLibertineTI", "LinLibertineOI", "LibertineMathMI", "Inconsolatazi4-Regular") # This last two are used in math envs and \verb|| envs.
 
 PLACEHOLDER_SHORTAUTHORS_SUBSTRING = "Surname"
 PLACEHOLDER_SHORTTILE_SUBSTRING = "Is Hope"
